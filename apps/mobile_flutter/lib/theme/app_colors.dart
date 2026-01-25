c07a3b2eb5bda4743844719e6620ef74ac87b35b
Gary Jurado <gjurado1@student.umgc.edu>
2026-01-25 14:13:03 +0100

    app color themes added

---
+++ apps/mobile_flutter/lib/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bgPrimary = Color(0xFFF5F7FA);
+  static const bgSurface = Colors.white;
+
+  static const textPrimary = Color(0xFF1A2332);
+  static const textSecondary = Color(0xFF4A5568);
+
+  static const border = Color(0xFFCBD5E0);
+
+  static const buttonPrimary = Color(0xFF4C6FBC);
+  static const buttonPrimaryHover = Color(0xFF3D5A9A);
+
+  static const statusSuccess = Color(0xFF2F855A);
+  static const statusWarning = Color(0xFFD69E2E);
+  static const statusError = Color(0xFFC53030);
+
+  static const alertInfoBg = Color(0xFFE6F2FF);
+  static const alertInfoBorder = Color(0xFF90C9FF);
+}
